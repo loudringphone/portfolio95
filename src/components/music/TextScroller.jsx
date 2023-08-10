30f893393b6bd8b64b7f8f1ebe4fbcf441b3c750
loudringphone <loudringphone@gmail.com>
2023-08-10 13:16:10 +1000

    add music countdown

--- src/components/music/TextScroller.jsx
+++ src/components/music/TextScroller.jsx
@@ -1,21 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSpring, animated } from "react-spring";
 
 const TextScroller = ({ text }) => {
   const [animationComplete, setAnimationComplete] = useState(false);
+  const [duration, setDuration] = useState(10000)
   const [key, setKey] = useState(1);
 
   const scrolling = useSpring({
     from: { transform: "translate(100%,0)" },
-    to: { transform: "translate(-200%,0)" },
-    config: { duration: 20000 },
+    to: { transform: "translate(-250%,0)" },
+    config: { duration: duration },
     reset: animationComplete,
     //reverse: key % 2 == 0,
     onRest: () => {
       setKey(key + 1);
-      setAnimationComplete(true);
     }
   });
+
+  useEffect(() => {
+    if (!animationComplete) {
+      setTimeout(() => {
+        setAnimationComplete(true)
+      }, duration);
+    } else {
+      setTimeout(() => {
+        setAnimationComplete(false)
+      }, 0);
+    }
+    
+  }, [animationComplete]); 
 
   return (
     <div key={key} className="music-title">
