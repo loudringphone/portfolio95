35334d4a3bd8d99242666cc05a910632eed8df36
loudringphone <loudringphone@gmail.com>
2023-08-11 01:22:06 +1000

    blue screen of love

---
+++ src/components/desktop-icons/Icon.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useRef} from 'react'
+import Draggable from 'react-draggable';
+
+
+const Icon = ({ task, icon, iconRef, visibility, handleIcon, handlePickingIcon, handleLeavingIcon, iconIndice }) => {
+  const [state, setState] = useState({
+    activeDrags: 0,
+    deltaPosition: {
+      x: 0, y: 0
+    },
+    controlledPosition: {
+      x: -400, y: 200
+    }
+  });
+  const onStart = () => {
+    setState(prevState => ({ ...prevState, activeDrags: prevState.activeDrags + 1 }));
+  };
+  const onStop = () => {
+    setState(prevState => ({ ...prevState, activeDrags: prevState.activeDrags - 1 }));
+  };
+  const dragHandlers = { onStart, onStop };
+  const taskName = task.split(' ').map((word) => word[0].toUpperCase() + word.slice(1)).join(' ')
+  return (
+    <Draggable bounds="body" {...dragHandlers}>
+    <div className='icon' style={{ zIndex: iconIndice[task], visibility: visibility, }}>
+      <div
+        ref={iconRef}
+        onDoubleClick={(event) => handleIcon(event, task)}
+        onTouchStart={(event) => handleIcon(event, task)}
+        onMouseDown={() => handlePickingIcon(task)}
+        onTouchEnd={() => handleLeavingIcon(task)}
+        onMouseUp={() => handleLeavingIcon(task)}
+        onClick={() => handleLeavingIcon(task)}
+      >
+        {icon}
+      </div>
+      <p
+        onDoubleClick={(event) => handleIcon(event, task)}
+        onTouchStart={(event) => handleIcon(event, task)}
+        onMouseDown={() => handlePickingIcon(task)}
+        onTouchEnd={() => handleLeavingIcon(task)}
+        onMouseUp={() => handleLeavingIcon(task)}
+        onClick={() => handleLeavingIcon(task)}
+      >
+        {taskName}
+      </p>
+    </div>
+  </Draggable>
+  )
+}
+
+export default Icon
