93c804665cfecdb36502e1a0ee0cd282bd720c46
loudringphone <loudringphone@gmail.com>
2024-01-07 19:47:50 +1100

    add close button to helper

--- src/components/welcome/Helper.jsx
+++ src/components/welcome/Helper.jsx
@@ -1,9 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
+import CloseFillIcon from 'remixicon-react/CloseFillIcon';
 
-const Helper = ({ helperDisplay }) => {
+const Helper = ({ helperDisplay, setHelperDisplay }) => {
+  const closeHelper = () => {
+    setHelperDisplay('none')
+  }
 
   return (
     <div className='helper' style={{ display: helperDisplay }}>
+      <div className='closeBtn' onClick={closeHelper} onTouch={closeHelper}><CloseFillIcon /></div>
       <p>User name: Admin</p>
       <p>Password: admin</p>
     </div>
