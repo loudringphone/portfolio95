7d93132385edeb5652ec5d1daeb0898c45c33183
loudringphone <loudringphone@gmail.com>
2024-01-13 15:44:46 +1100

    test disable drag down to refresh

--- src/components/WindowComponent.jsx
+++ src/components/WindowComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Window } from 'react95';
 
 const WindowComponent = ({ task, setActiveTask, indexingWindows, icons, setSelectedBinIcon, children }) => {
@@ -15,14 +15,41 @@ const WindowComponent = ({ task, setActiveTask, indexingWindows, icons, setSelec
       setSelectedBinIcon(null)
     }
   };
-  
   const handleMouseDown = () => {
     setActiveTask(task);
     indexingWindows(task);
   }
 
+  const [documentPosition, setDocumentPosition] = useState(0);
+  const [touchStartY, setTouchStartY] = useState(null);
+  const handleTouchStart = (event) => {
+    setTouchStartY(event.touches[0].clientY);
+    setDocumentPosition(document.documentElement.scrollTop);
+  };
+  const handleTouchMove = (event) => {
+    const touchEndY = event.touches[0].clientY;
+    if (setDocumentPosition === 0 && touchEndY > touchStartY) {
+      event.preventDefault();
+    }
+  };
+  useEffect(() => {
+    const logValues = () => {
+      console.log('Document position:', document.documentElement.scrollTop);
+    };
+
+    window.addEventListener('touchstart', handleTouchStart);
+    window.addEventListener('touchmove', handleTouchMove, { passive: false });
+
+    return () => {
+      logValues(); // Log values before cleanup
+
+      window.removeEventListener('touchstart', handleTouchStart);
+      window.removeEventListener('touchmove', handleTouchMove)
+    };
+  }, [touchStartY, documentPosition]);
+
   return (
-    <Window className={`${task.replaceAll(' ', '-')}-window`} onClick={handleClickInsideWindow} onMouseDown={handleMouseDown}>
+    <Window className={`${task.replaceAll(' ', '-')}-window`} onClick={handleClickInsideWindow} onMouseDown={handleMouseDown} onTouchStartCapture={handleTouchStart} onTouchMove={handleTouchMove}>
       {children}
     </Window>
   )
