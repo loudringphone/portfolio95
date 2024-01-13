5f13a42f2895402992317c3b42f871c761795e26
loudringphone <loudringphone@gmail.com>
2024-01-13 18:34:35 +1100

    remove empty space

--- src/components/warning/WarningWindow.jsx
+++ src/components/warning/WarningWindow.jsx
@@ -40,42 +40,41 @@ const WarningWindow = ({displayTasks, displayingTask, setActiveTask, activeTask,
     }
   }, [warnings])
   
-
   return (
     <DraggableComponent task={task} initialPosition={initialPosition} setActiveTask={setActiveTask} indexingWindows={indexingWindows}>
-    <Wrapper className="drag-warning" style={{zIndex: windowIndice.warning, display: displayTasks.has(task) ? 'block' : 'none'}}>
-    <WindowComponent task={task} setActiveTask={setActiveTask} indexingWindows={indexingWindows}>
-    <strong className="cursor"><WindowHeader  active={activeTask == task} className='window-title'>
-        <span>Warning</span>
-        <div className="buttons">
-        <Button disabled={true} active={false}>
-          <CloseFillIcon />
-        </Button>
-        </div>
-      </WindowHeader></strong>
-      <WindowContent className='window-content'>
-        <div className='warning'>
-        <img
-            src={warning}
-            alt="warning"
-            style={{ height: "40px"}}
-            />
-            {
-              warnings >= 3 ?
-              <p>Winston can help fix the bugs for you!</p>
-              :
-              warnings >= 2 ?
-              <p>Hiring Winston would be a good decision!</p>
-              :
-              <p>Confirm discarding Winston's resume?</p>
-            }
-        </div>
-        <Button onClick={handleClick} onTouchStart={handleClick}> {warnings >= 2 ? 'Yes' :'No'}
-        </Button>
-      </WindowContent>
-    </WindowComponent>
-  </Wrapper>
-  </DraggableComponent>
+      <Wrapper className="drag-warning" style={{zIndex: windowIndice.warning, display: displayTasks.has(task) ? 'block' : 'none'}}>
+        <WindowComponent task={task} setActiveTask={setActiveTask} indexingWindows={indexingWindows}>
+          <strong className="cursor"><WindowHeader  active={activeTask == task} className='window-title'>
+            <span>Warning</span>
+            <div className="buttons">
+              <Button disabled={true} active={false}>
+                <CloseFillIcon />
+              </Button>
+            </div>
+          </WindowHeader></strong>
+          <WindowContent className='window-content'>
+            <div className='warning'>
+              <img
+                src={warning}
+                alt="warning"
+                style={{ height: "40px"}}
+              />
+              {
+                warnings >= 3 ?
+                <p>Winston can help fix the bugs for you!</p>
+                :
+                warnings >= 2 ?
+                <p>Hiring Winston would be a good decision!</p>
+                :
+                <p>Confirm discarding Winston's resume?</p>
+              }
+            </div>
+            <Button onClick={handleClick} onTouchStart={handleClick}> {warnings >= 2 ? 'Yes' :'No'}
+            </Button>
+          </WindowContent>
+        </WindowComponent>
+      </Wrapper>
+    </DraggableComponent>
   )
 }
 
